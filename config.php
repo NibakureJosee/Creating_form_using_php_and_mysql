0f27ac40748802ffe84886616c58b91c2d3e182b
Josephine <joseenibakure@gmail.com>
2022-02-25 16:05:08 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'users');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($link === FALSE){
+    die(" ERROR: Could not connect." .mysqli_connect_error());
+}
+
+?>
